66e3db68829f9b5c7479223cf2bac75f74593c2d
Roman <mrroman6552@gmail.com>
2021-08-31 15:14:50 +0300

    refactor: moved the task rendering logic to the App

--- src/components/AppMain/AppMain.js
+++ src/components/AppMain/AppMain.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./AppMain.css";
 import TodoListItems from "../TodoListItems/TodoListItems";
 import Filter from "../Filter/Filter";
 import CounterTodos from "../CounterTodos/CounterTodos";
+import AddSection from "../AddSection/AddSection";
+import TodoItem from "../TodoItem/TodoItem";
+import { initialTodoData } from "./../../utils/utils";
+
+let newTodoId = 100; // начало отсчета id новых задач (для передачи его в key)
 
 function AppMain() {
+  const [todoData, setTodoData] = useState(initialTodoData);
+
+  console.log("todoData :>> ", todoData);
+  console.log("newTodoId :>> ", newTodoId);
+
+  function renderTodoItems() {
+    return todoData.map((item) => {
+      const { id, title, important } = item;
+      return (
+        <TodoItem
+          key={id}
+          title={title}
+          important={important}
+          onDeleted={() => handleDeletItem(id)}
+        />
+      );
+    });
+  }
+
+  function handleDeletItem(id) {
+    setTodoData((currTodos) => {
+      const targetIndex = currTodos.findIndex((item) => item.id === id);
+      const newTodosBefore = currTodos.slice(0, targetIndex);
+      const newTodosAfter = currTodos.slice(targetIndex + 1);
+
+      return [...newTodosBefore, ...newTodosAfter];
+    });
+  }
+
+  function handleAddTodo(text) {
+    setTodoData((currTodos) => {
+      return [
+        ...currTodos,
+        {
+          id: newTodoId++,
+          title: text.trim(),
+          important: false,
+        },
+      ];
+    });
+  }
+
   return (
     <main className="main">
       <article>
         <section>
           <Filter />
           <CounterTodos />
-          <TodoListItems />
+          <TodoListItems renderTodoItems={renderTodoItems} />
+          <AddSection handleAddTodo={handleAddTodo} />
         </section>
       </article>
     </main>
--- src/components/TodoListItems/TodoListItems.js
+++ src/components/TodoListItems/TodoListItems.js
@@ -1,70 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./TodoListItems.css";
-import TodoItem from "../TodoItem/TodoItem";
-import todoAddIcon from "../../images/svg/task_add_24dp.svg";
-import { initialTodoData } from "./../../utils/utils";
 
-let newTodoId = 100; // начало отсчета id новых задач (для передачи его в key)
-
-function TodoListItems() {
-  const [todoData, setTodoData] = useState(initialTodoData);
-
-  console.log("todoData :>> ", todoData);
-  console.log("newTodoId :>> ", newTodoId);
-
-  function renderTodoItems() {
-    return todoData.map((item) => {
-      const { id, title, important } = item;
-      return (
-        <TodoItem
-          key={id}
-          title={title}
-          important={important}
-          onDeleted={() => handleDeletItem(id)}
-        />
-      );
-    });
-  }
-
-  function handleDeletItem(id) {
-    setTodoData((currTodos) => {
-      const targetIndex = currTodos.findIndex((item) => item.id === id);
-      const newTodosBefore = currTodos.slice(0, targetIndex);
-      const newTodosAfter = currTodos.slice(targetIndex + 1);
-
-      return [...newTodosBefore, ...newTodosAfter];
-    });
-  }
-
-  function handleAddTodo(text) {
-    console.log("text :>> ", text);
-
-    setTodoData((currTodos) => {
-      return [
-        ...currTodos,
-        {
-          id: newTodoId++,
-          title: text.trim(),
-          important: false,
-        },
-      ];
-    });
-  }
-
+function TodoListItems({ renderTodoItems }) {
   return (
-    <>
+    <section>
       <ul className="todo-list-items">{renderTodoItems()}</ul>
-
-      <button
-        type="button"
-        className="todo-item__btn"
-        onClick={() => handleAddTodo("hello world")}
-      >
-        <img src={todoAddIcon} alt="add todo." className="todo-item__icon" />
-        <div className="todo-item__btn-hint">add Todo</div>
-      </button>
-    </>
+    </section>
   );
 }
 
