64622071be8411331f085c818a4db43ee7da3bb6
Roman <mrroman6552@gmail.com>
2021-09-03 21:17:04 +0300

    feat: create AddSection form and handleSubmitAddForm fn

--- src/components/AddSection/AddSection.js
+++ src/components/AddSection/AddSection.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
+import "./AddSection.css";
 import todoAddIcon from "../../images/svg/task_add_24dp.svg";
 
 function AddSection({ onAddItem }) {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChangeAddInput(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmitAddForm(e) {
+    e.preventDefault();
+    setInputValue("");
+    e.target.reset();
+
+    onAddItem(inputValue);
+  }
+
   return (
     <section>
-      <button
-        type="button"
-        className="todo-item__btn"
-        onClick={() => onAddItem("        hello      world")}
-      >
-        <img src={todoAddIcon} alt="add todo." className="todo-item__icon" />
-        <div className="todo-item__btn-hint">add Todo</div>
-      </button>
+      <form className="add-section__form" onSubmit={handleSubmitAddForm}>
+        <input
+          type="text"
+          className="add-section__input"
+          placeholder="Add Todo"
+          autoComplete="off"
+          onChange={handleChangeAddInput}
+        />
+
+        <button type="submit" className="todo-item__btn">
+          <img
+            src={todoAddIcon}
+            alt="add todo."
+            className="add-section__icon"
+          />
+          <div className="todo-item__btn-hint">add Todo</div>
+        </button>
+      </form>
     </section>
   );
 }
